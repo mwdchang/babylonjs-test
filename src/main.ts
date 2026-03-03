c3fb7a49018dbc9140b78f6c6ad119ab5425b758
Daniel Chang <mwdchang@gmail.com>
2026-03-03 09:49:28 -0500

    multiple segments

--- src/main.ts
+++ src/main.ts
@@ -18,20 +18,40 @@ const text = new TextBlock();
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 const engine = new Engine(canvas, true);
 
+// Configurations
+const GRID_SIZE = 20;
 const CAMERA_POS = new Vector3(20, 20, 20);
 const GRID_COLOR = new Color3(0.5, 0.5, 0.5);
 const GOAL_COLOR = new Color3(0.2, 0.9, 0.1);
+const NUM_BLOCKS = 20;
+const RIBBON_WIDTH = 0.1;
 
-const NUM_BLOCKS = 60;
-const RIBBON_WIDTH = 0.2;
+
+// Interaction
+const selectedBlock = {
+  material: StandardMaterial,
+  color: Color3
+};
 
 let tempMaterial: StandardMaterial;
 let tempColor: Color3;
+
 let tempX = 0;
 let tempY = 0;
 let isDragging = true;
 const PICKED_COLOR = new Color3(0.9, 0.9, 0.1);
 
+
+function randomNumbers(len: number) {
+  let values = Array.from({ length: len }, () => Math.random());
+
+  // Normalize so they sum to 1
+  const sum = values.reduce((a, b) => a + b, 0);
+  return values.map(v => v / sum);
+}
+
+
+// Add a ribbon that runs along points
 const addRibbon = (points: { x: number, y: number, z: number }[], color: Color3, scene: Scene) => {
   const vector3Points = points.map(p => new Vector3(p.x, p.y, p.z));
   const curve = Curve3.CreateCatmullRomSpline(vector3Points, 10, false);
@@ -66,6 +86,7 @@ const addRibbon = (points: { x: number, y: number, z: number }[], color: Color3,
   return ribbon;
 }
 
+// Create scene graph
 const createScene = (): Scene => {
   const scene = new Scene(engine);
   scene.clearColor = new Color4(0, 0, 0, 1);
@@ -77,12 +98,11 @@ const createScene = (): Scene => {
   const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
   light.intensity = 1.2;
 
-  const gridSize = 20;
 
   const grid: GroundMesh = MeshBuilder.CreateGround("grid", {
-    width: gridSize,
-    height: gridSize,
-    subdivisions: gridSize
+    width: GRID_SIZE,
+    height: GRID_SIZE,
+    subdivisions: GRID_SIZE 
   }, scene);
   const gridMaterial = new GridMaterial("gridMaterial", scene);
   gridMaterial.majorUnitFrequency = 5;
@@ -91,6 +111,7 @@ const createScene = (): Scene => {
   gridMaterial.lineColor = GRID_COLOR;
   gridMaterial.backFaceCulling = false; // Disable back-face culling for the grid
   grid.material = gridMaterial;
+  grid.position.y = -0.001;
 
 
   const ribbonPoints = [];
@@ -98,19 +119,13 @@ const createScene = (): Scene => {
   const dupes = new Set<string>();
 
   for (let i = 0; i < NUM_BLOCKS; i++) {
-    const blockMaterial = new StandardMaterial("blockMaterial", scene);
-    blockMaterial.diffuseColor = new Color3(0.8, 0.8, 0.8);
-    blockMaterial.alpha = 0.9;
-
-
     let height = Math.random() * 5 + 1;
 
-    const x = Math.floor(Math.random() * gridSize) - gridSize / 2 + 0.5;
-    const z = Math.floor(Math.random() * gridSize) - gridSize / 2 + 0.5;
+    const x = Math.floor(Math.random() * GRID_SIZE) - GRID_SIZE / 2 + 0.5;
+    const z = Math.floor(Math.random() * GRID_SIZE) - GRID_SIZE / 2 + 0.5;
 
     const key = `${x}:${z}`;
     if (dupes.has(key)) {
-      console.log(key);
       continue;
     } else {
       dupes.add(key);
@@ -119,27 +134,86 @@ const createScene = (): Scene => {
     if (Math.abs(x) < 3 && Math.abs(z) < 3) {
       height = Math.random() * 12 + 3;
     }
-    const block = MeshBuilder.CreateBox("box", { size: 0.5, height: height }, scene);
-    block.metadata = {
-      id: i,
-      height: height
-    };
 
-    block.position = new Vector3(x, height / 2, z);
+    const blockMaterial = new StandardMaterial("blockMaterial", scene);
+    blockMaterial.diffuseColor = height > 5 ? GOAL_COLOR : new Color3(0.8, 0.8, 0.8);
+    blockMaterial.alpha = 0.9;
+
+
+    // Tapered block (top is smaller)
+    const ratios = randomNumbers(4)
+    let posY = 0;
+
+    for (let i = 0; i < ratios.length; i++) {
+      const ratio = ratios[i];
+
+      // anchored at center
+      if (i === 0) {
+        posY = 0.5 * (height * ratio);
+      } else {
+        posY += 0.5 * (height * ratios[i - 1]) + 0.5 * (height * ratio);
+      }
+
+      const block = MeshBuilder.CreateCylinder("taperedCube", {
+          height: height * ratio,
+          diameterTop: 0.2 + Math.random() * 0.8,
+          diameterBottom: 0.2 + Math.random() * 0.8,
+          tessellation: 6
+      }, scene);
+      block.position = new Vector3(x, posY, z);
+      block.material = blockMaterial;
+    }
+
+    
+    /*
+    const testRandom = 0.75;
+    const block = MeshBuilder.CreateCylinder("taperedCube", {
+        height: height * (testRandom),
+        diameterTop: 0.25,
+        diameterBottom: 0.75,
+        tessellation: 6
+    }, scene);
+    // block.position = new Vector3(x, height / 2, z);
+    block.position = new Vector3(x, (height * testRandom) / 2, z);
     block.material = blockMaterial;
 
+
+    
+    const block2 = MeshBuilder.CreateCylinder("taperedCube", {
+        height: height * (1 - testRandom),
+        diameterTop: 0.55,
+        diameterBottom: 0.75,
+        tessellation: 6
+    }, scene);
+    // block2.position = new Vector3(x, height / 2, z);
+    block2.position = new Vector3(x, (height / 2) + 1.5 * (height * (1 - testRandom)), z);
+    block2.material = blockMaterial;
+    */
+   
+
+
+
+
+    // Regular block
+    // const block = MeshBuilder.CreateBox("box", { size: 0.5, height: height }, scene);
+    // block.position = new Vector3(x, height / 2, z);
+    // block.metadata = {
+    //   id: i,
+    //   height: height
+    // };
+
+
     if (height > 5) {
-      blockMaterial.diffuseColor = GOAL_COLOR;
       ribbonPoints.push({
         x, y: height, z
       });
     }
   }
 
+  // Sort from highest to lowest
   ribbonPoints.sort((a, b) => {
     return b.y - a.y;
   });
-
   addRibbon(ribbonPoints, GOAL_COLOR, scene);
 
 
@@ -182,12 +256,6 @@ const createScene = (): Scene => {
             tempMaterial = pickedMaterial as StandardMaterial;
           }
 
-          /*
-          const pickedMesh = pickResult.pickedMesh as Mesh & { _blockIndex?: number };
-          if (pickedMesh._blockIndex !== undefined) {
-            console.log("Block clicked: Index =", pickedMesh._blockIndex, "Name =", pickedMesh.name);
-          }
-          */
         } else {
           if (tempMaterial) {
             (tempMaterial as StandardMaterial).diffuseColor = tempColor;
@@ -212,10 +280,8 @@ const createScene = (): Scene => {
   return scene;
 };
 
+
 const scene = createScene();
-
-
-
 engine.runRenderLoop(function () {
   scene.render();
 });
